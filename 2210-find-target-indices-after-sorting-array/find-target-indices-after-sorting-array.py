b8b2c6731e0aaf2cb2f73e63ac4b9b26762bc649
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-08-02 14:06:34 +0300

    Time: 44 ms (78.91%) | Memory: 16.3 MB (99.62%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
@@ -1,28 +1,8 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        min_val = min(nums)
-        max_val = max(nums)
-        range_val = max_val - min_val + 1
-        count = [0] * range_val
-    
-        # Count the occurrences of each number
-        for num in nums:  
-            count[num - min_val] += 1
-    
-        # Update the count array to store the cumulative counts
-        for i in range(1, range_val):
-            count[i] += count[i - 1]
-    
-        sorted_arr = [0] * n
-    
-        # Place the elements in sorted order
-        for j in range(n):
-            index = count[nums[j] - min_val] - 1  # Subtract 1 for zero-based index
-            sorted_arr[index] = nums[j]
-            count[nums[j] - min_val] -= 1
+        nums.sort()
         output = []
-        for i in range(n):
-            if sorted_arr[i] == target:
+        for i in range(len(nums)):
+            if(nums[i] == target):
                 output.append(i)
         return output
