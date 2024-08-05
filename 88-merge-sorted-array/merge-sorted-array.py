faf9357b1f8513bb3a4cd06d4099c35fce882be8
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-08-05 19:43:40 +0300

    Time: 43 ms (33.33%) | Memory: 16.5 MB (45.65%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,18 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        p1 = m - 1
-        p2 = n - 1
-        p = n + m - 1 
+        p1,p2 = m - 1, n - 1
+        p = m + n -1
         while(p1 >= 0 and p2 >= 0):
-            if nums1[p1] > nums2[p2]:
+            if(nums1[p1] > nums2[p2]):
                 nums1[p] = nums1[p1]
                 p1 -= 1
             else:
                 nums1[p] = nums2[p2]
                 p2 -= 1
             p -= 1
-        # if there are remaining elements in nums2
+        # handling the remaining elements
         while(p2 >= 0):
             nums1[p] = nums2[p2]
             p2 -= 1
