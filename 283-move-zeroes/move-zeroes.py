dd14b6df3029127822ddc305762e248d19bb0381
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-08-17 18:59:34 +0300

    Time: 119 ms (85.73%) | Memory: 18.1 MB (20.32%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,10 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        p1 = 0
+        non_zero_idx = 0
         n = len(nums)
+        
         for i in range(n):
             if nums[i] != 0:
-                nums[p1],nums[i] = nums[i],nums[p1]
-                p1 += 1
-        return nums
+                nums[i],nums[non_zero_idx] = nums[non_zero_idx],nums[i]
+                non_zero_idx += 1
+        # time complexity = O(n)
+        # space complexity = O(1)
