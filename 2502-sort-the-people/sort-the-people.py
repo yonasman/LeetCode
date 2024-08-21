be2064d1883d86cf9b2cc8f848d4e46b5da01a9f
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-08-21 13:23:02 +0300

    Time: 752 ms (5.01%) | Memory: 17 MB (91.98%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,26 +1,14 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         n = len(heights)
-        max_hgt = max(heights)
-        count_arr = [0] * (max_hgt + 1)
-        
-        for h in heights:
-            count_arr[h] += 1
-            
-        for i in range(1,max_hgt + 1):
-            count_arr[i] += count_arr[i - 1]
-        
-        output_names = [0] * n
-        output_heights = [0] * n
-        
-        i = n - 1
-        
-        while(i >= 0):
-            h = heights[i]
-            pos = count_arr[h] - 1
-            output_heights[pos] = h
-            output_names[pos] = names[i]
-            count_arr[h] -= 1
-            i -= 1
-        output_names.reverse()
-        return output_names
+        swap = False
+        for i in range(n):
+            for j in range(n - i - 1):
+                if heights[j] < heights[j + 1]:
+                    heights[j], heights[j + 1] = heights[j + 1], heights[j]
+                    names[j], names[j+1] = names[j+1], names[j]
+                    swap = True
+        if not swap:
+            return names
+        return names
+                
