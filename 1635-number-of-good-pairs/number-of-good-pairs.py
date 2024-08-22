3d0ded49a06a4d9b3093e6cb5b4d357dd4d8ea40
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-08-22 16:40:06 +0300

    Time: 35 ms (69.98%) | Memory: 16.5 MB (20.56%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -1,16 +1,13 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        # dictionary to count the number of unique items
-        d = {}
-        numOfIdentical = 0
+        freq = {}
+        count = 0
+        
         for num in nums:
-            if num in d:
-                d[num] += 1
+            if num in freq:
+                count += freq[num]
+                freq[num] += 1
             else:
-                d[num] = 1
-        for value in d.values():
-            if value > 1:
-                numOfIdentical += (value * (value - 1)) // 2
-        return numOfIdentical
-        # time complexity O(n)
-        # space complexity O(n)
+                freq[num] = 1
+        return count
+            
