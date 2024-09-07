f745a66f07f7084af9c57f8c7bce653dcaff73df
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-09-07 09:57:20 +0300

    Time: 44 ms (96.15%) | Memory: 16.7 MB (23.02%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,9 +1,20 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        sortedNums = sorted(nums)
         n = len(nums)
-        output = []
+        d = {}
         
-        for num in nums:
-            output.append(sortedNums.index(num))
+        for n in nums:
+            d[n] = d.get(n,0) + 1
+            
+        sortedNums = sorted(d.keys())
+        
+        cummulative_sum = 0
+        cummulative_count = {}
+        
+        for num in sortedNums:
+            cummulative_count[num] = cummulative_sum
+            cummulative_sum += d[num]
+        # output
+        output = [cummulative_count[n] for n in nums]
         return output
+            
