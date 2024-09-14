0f22e92b8dd51285ff1b8cf7290f1e8bf680fdb4
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-09-14 13:24:52 +0300

    Time: 45 ms (75.85%) | Memory: 16.8 MB (91.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,9 +1,4 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dict1 = {}
-        dict2 = {}
-        for str in s:
-            dict1[str] = dict1.get(str,0) + 1
-        for str in t:
-            dict2[str] = dict2.get(str,0) + 1
-        return dict1 == dict2
+        return Counter(s) == Counter(t)
