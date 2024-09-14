e1606be8d148595aa26a50cc8d5eaa750e8454a1
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-09-14 13:08:50 +0300

    Time: 47 ms (67.32%) | Memory: 16.9 MB (46.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        dict1 = {}
+        dict2 = {}
+        for str in s:
+            dict1[str] = dict1.get(str,0) + 1
+        for str in t:
+            dict2[str] = dict2.get(str,0) + 1
+        return dict1 == dict2
