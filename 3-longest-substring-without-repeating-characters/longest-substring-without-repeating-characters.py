c28c0087acd6e768d998930de0771ef49249bdd8
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-09-15 10:35:48 +0300

    Time: 51 ms (77.09%) | Memory: 16.5 MB (80.76%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,17 +3,13 @@ class Solution:
         n = len(s)
         if n == 0:
             return 0
-        left = 0
-        right = 0
+        p1 = 0
         maxLen = 0
         seen = set()
-        while left < n and right < n:
-            if s[right] not in seen:
-                seen.add(s[right])
-                maxLen = max(maxLen, right - left + 1)
-                right += 1
-            else:
-                seen.remove(s[left])
-                left += 1
-        return maxLen    
-            
+        for p2 in range(n):
+            while s[p2] in seen:
+                seen.remove(s[p1])
+                p1 += 1
+            seen.add(s[p2])
+            maxLen = max(maxLen, p2 - p1 + 1)
+        return maxLen
