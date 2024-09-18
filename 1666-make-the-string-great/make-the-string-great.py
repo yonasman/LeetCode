6a77a4500a3a8f8effc07807e20d2bbef35be1ab
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-09-18 11:30:03 +0300

    Time: 31 ms (91.56%) | Memory: 16.6 MB (28.15%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -0,0 +1,11 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        if not s:
+            return ""
+        stack = []
+        for char in s:
+            if stack and stack[-1].swapcase() == char:
+                stack.pop()
+            else:
+                stack.append(char)
+        return ''.join(stack)
