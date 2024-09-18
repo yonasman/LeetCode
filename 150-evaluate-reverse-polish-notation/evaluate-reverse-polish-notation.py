ccf6ae8229f4cf9560739dd3294daa1f200400ff
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-09-18 14:35:30 +0300

    Time: 63 ms (71.73%) | Memory: 17.1 MB (11.97%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,21 +1,21 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        # initialize a stack to track the tokens
         stack = []
-        # iterate over the tokens and perform the operation
         for token in tokens:
-            if token == '+':
-                stack[-2] = stack[-2] + stack[-1]
-                stack.pop()
-            elif token == '-':
-                stack[-2] = stack[-2] - stack[-1]
-                stack.pop()
-            elif token == '*':
-                stack[-2] = stack[-2] * stack[-1]
-                stack.pop()
-            elif token == '/':
-                stack[-2] = int(stack[-2] / stack[-1])
-                stack.pop()
-            else:
+            if token not in {'+','-','*','/'}:
                 stack.append(int(token))
+            else:
+                num1 = int(stack.pop())
+                num2 = int(stack.pop())
+                if token == '+':
+                    stack.append(num1 + num2)
+                elif token == '-':
+                    stack.append(num2 - num1)
+                elif token == '*':
+                    stack.append(num1 * num2)
+                else:
+                    if num2 * num1 < 0 and num2 % num1 != 0:
+                        stack.append((num2 // num1) + 1)
+                    else:
+                        stack.append(num2 // num1)
         return stack[-1]
