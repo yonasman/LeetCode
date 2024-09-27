eda77de67d0f221ba529c7334602235f83a00906
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-09-27 22:53:39 +0300

    Time: 40 ms (79.73%) | Memory: 16.5 MB (33.71%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
@@ -1,9 +1,7 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
-        num_set = set(nums)
-        number_of_triplets = 0
-        
+        numOfTriplets = 0
         for num in nums:
-            if (num + diff) in num_set and (num + 2 * diff) in num_set:
-                number_of_triplets += 1
-        return number_of_triplets
+            if (num - diff) in nums and (num + diff) in nums:
+                numOfTriplets += 1
+        return numOfTriplets
